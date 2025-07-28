dfc8a410ef76b09187f973d58acb1444d0b69fe3
abemka1234 <alesparfenov@gmail.com>
2025-07-28 17:15:37 +0300

    Публикация файлов проекта

---
+++ settings.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.getenv("HOST"),
+        'PORT': os.getenv("PORT"),
+        'NAME': os.getenv("NAME"),
+        'USER': os.getenv("USER"),
+        'PASSWORD': os.getenv("PASSWORD")
+    }
+}
+
+INSTALLED_APPS = ['datacenter']
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_TZ = True
